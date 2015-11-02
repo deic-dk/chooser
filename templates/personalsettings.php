74a6e8c4ed33419eb0b29f3f6d8b46dead34bdfb
Lars Næsbye Christensen <lars@naesbye.dk>
2015-11-02 13:13:46 +0100

    Simple typo fix in headline

--- templates/personalsettings.php
+++ templates/personalsettings.php
@@ -1,5 +1,5 @@
 <fieldset class="section">
-	<h2><?php p($l->t('WebDAV sccess control')); ?></h2>
+	<h2><?php p($l->t('WebDAV access control')); ?></h2>
 	<?php p($l->t('Allow unauthenticated WebDAV access from compute nodes')); ?>
 	<input type="checkbox" name="allow_internal_dav" id="allow_internal_dav" value="0"
 		   title="<?php p($l->t( 'Needed for file access from virtual machines.' )); ?>"
