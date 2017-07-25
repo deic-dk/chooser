51b230dc864ad2b7c1b9e01829e1b4936008d9fb
deltafunction <frederik@orellana.dk>
2017-07-25 16:14:25 +0200

    Comment.

--- appinfo/apache_note_user.php
+++ appinfo/apache_note_user.php
@@ -1,6 +1,8 @@
 <?php
 
 // This is for apache to use for logging.
+// Like e.g.
+// LogFormat "%h %l %u %{username}n %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
 if(\OC_User::isLoggedIn()){
 	apache_note( 'username', \OC_User::getUser() );
 }
