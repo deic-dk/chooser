c265eae9440b0a776752f78f55e7ea6c0923ae5b
deltafunction <frederik@orellana.dk>
2018-04-05 23:01:21 +0200

    Localization.

---
+++ l10n/da.php
@@ -0,0 +1,10 @@
+<?php
+$TRANSLATIONS = array(
+		"Save" => "Gem",
+		"Data processing access control" => "Adgangskontrol for databehandling",
+		"Allow access from your own compute nodes" => "Tillad adgang fra egne beregningsnoder",
+		"Needed for file access from your virtual machines" => "Nødvendig for at dine virtuelle maskiner kan tilgå dine data",
+		"Allow authentication with your personal X.509 certificate:" => "Tillad autentificering med personligt X.509-certifikat",
+		"Certificate subject" => "Certifikatets DN",
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- templates/personalsettings.php
+++ templates/personalsettings.php
@@ -1,19 +1,19 @@
 <?php OCP\Util::addStyle('chooser', 'personalsettings');?>
 
 <fieldset class="section">
-	<h2><?php p($l->t('WebDAV access control')); ?></h2>
+	<h2><?php p($l->t('Data processing access control')); ?></h2>
 	
-	<?php p($l->t('Allow unauthenticated WebDAV access from compute nodes')); ?>
+	<?php p($l->t('Allow access from your own compute nodes')); ?>
 	<input type="checkbox" id="allow_internal_dav" value="0"
-		   title="<?php p($l->t( 'Needed for file access from your virtual machines.' )); ?>"
+		   title="<?php p($l->t( 'Needed for file access from your virtual machines' )); ?>"
 		   <?php if ($_['is_enabled'] == 'yes'): ?> checked="checked"<?php endif; ?> />
 		   
 	<br />
 	
-	<?php p($l->t('Allow authentication with X.509 certificate: ')); ?>
+	<?php p($l->t('Allow authentication with your personal X.509 certificate:').' '); ?>
 	<input type="text" id="ssl_cert_dn"
 		value="<?php print(isset($_['ssl_cert_dn'])?$_['ssl_cert_dn']:''); ?>"
-		placeholder="Certificate subject" />
-		<label id="chooser_settings_submit" class="button">Save</label>&nbsp;<label id="chooser_msg"></label>
+		placeholder="<?php p($l->t('Certificate subject'));?>" />
+		<label id="chooser_settings_submit" class="button"><?php p($l->t("Save"));?></label>&nbsp;<label id="chooser_msg"></label>
 </fieldset>
 
